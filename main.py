7a3b784c510bb055c76f040eda218f50c05f6f2a
Simon Fong <simonfong6@gmail.com>
2017-11-29 13:44:08 -0800

    Loaded inception and evaluated 1 image

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from inception_v3 import inception_v3_base
+from inception_v3 import inception_v3
 import tensorflow as tf
+import cv2
+import numpy as np
+
+image = cv2.imread('256_ObjectCategories/001.ak47/001_0001.jpg')
+
+small = cv2.resize(image, (299,299))
 
 
-inputs = tf.placeholder("float", [10,100,100,3])
-base = inception_v3_base(inputs)
+
+
+
+inputs = tf.placeholder("float", [None,299,299,3])
+y_ = tf.placeholder("float", [None,1,1,256])
+
+y = np.zeros((1,1,256))
+
+base, some = inception_v3(inputs, num_classes=256)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    poop = base.eval(feed_dict={inputs: [small], y_: [y]})
+    print poop
