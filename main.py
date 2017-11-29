f42c76e762d27dd53e976bcfadd3a94dc8e1b2e0
Simon Fong <simonfong6@gmail.com>
2017-11-29 14:04:04 -0800

    Turned nums into CONSTANTS

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 import cv2
 import numpy as np
 
+IMAGE_WIDTH = 299
+IMAGE_HEIGHT = 299
+NUM_CLASSES = 256
+
 image = cv2.imread('256_ObjectCategories/001.ak47/001_0001.jpg')
 
 small = cv2.resize(image, (299,299))
@@ -11,12 +15,12 @@ small = cv2.resize(image, (299,299))
 
 
 
-inputs = tf.placeholder("float", [None,299,299,3])
-y_ = tf.placeholder("float", [None,1,1,256])
+inputs = tf.placeholder("float", [None,IMAGE_HEIGHT,IMAGE_WIDTH,3])
+y_ = tf.placeholder("float", [None,1,1,NUM_CLASSES])
 
-y = np.zeros((1,1,256))
+y = np.zeros((1,1,NUM_CLASSES))
 
-base, some = inception_v3(inputs, num_classes=256)
+base, some = inception_v3(inputs, num_classes=NUM_CLASSES)
 
 init = tf.global_variables_initializer()
 
