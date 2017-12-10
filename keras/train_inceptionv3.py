1dd0470405015b7f41598fab03795269c21ac367
Simon Fong <simonfong6@gmail.com>
2017-12-10 00:39:24 -0800

    Reorganized repo

--- keras/train_inceptionv3.py
+++ keras/train_inceptionv3.py
@@ -2,7 +2,6 @@
 """
 COGS 118B Project
 Author: Simon Fong, Thinh Le, Wilson Tran
-
 """
 
 from keras.models import Model
@@ -11,14 +10,13 @@ from keras import optimizers
 from keras.layers import Dropout, Flatten, Dense
 from keras.utils.np_utils import to_categorical
 import numpy as np
-import glob
 import os
 import cv2
 import random
 import matplotlib
 matplotlib.use('Agg')                           # Stops from plotting to screen
 import matplotlib.pyplot as plt
-from dataset import Dataset                     # Custom Datset
+from dataset import Dataset                     # Custom Dataset
 
 
 IMAGE_WIDTH,IMAGE_HEIGHT,NUM_CHANNELS = 299,299,3
@@ -35,77 +33,62 @@ cal.read_data()
 cal.train_val_test_split(NUM_TRAIN,NUM_VAL,NUM_TEST)
 num_classes = cal.num_classes
 
-MEAN_PIXEL = np.array([104., 117., 123.]).reshape((1,1,3))
-
-
 def load_model():
+    """Returns a pretrained model"""
+    
+    # Loads base model
     base_model = InceptionV3(include_top=False, weights='imagenet',input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS))
-
-    print('Model weights loaded.')
+    print("Model weights loaded.")
+    
     base_out = base_model.output
 
+    # Add layers more layers
     x = Flatten()(base_out)
     x = Dense(256,activation='relu')(x)
     x = Dropout(0.5)(x)
 
+    # Final fully connected layer to work with our data
     predictions = Dense(num_classes,activation='softmax')(x)
 
+    # Build a final model
     model = Model(inputs=base_model.input, outputs=predictions)
-    print 'Build model'
+    
+    print("Model structure")
     model.summary()
 
-    model.compile(optimizers.SGD(lr=1e-4,momentum=0.9),'categorical_crossentropy', metrics=['accuracy'])
 
-    print 'Compile model'
+    model.compile(optimizers.SGD(lr=1e-4,momentum=0.9),
+                'categorical_crossentropy', metrics=['accuracy'])
+    print("Model compiled")
+
     return model
 
 
 
 
 def main():
-    # make model
+    
+    # Make model
     model = load_model()
-    print 'Inception created\n'
+    print("Inception created\n")
 
-    num_steps = int(cal.image_count / BATCH_SIZE/16)
-
-    # Store data to plot
+    # Init data array to plot
     train_acc = np.array([])
     train_val_acc = np.array([])
     train_loss = np.array([])
     train_val_loss = np.array([])
-    """
-    print "NUM STEPS = {}".format(num_steps)
-
-    for i in range(num_steps):
-        print('STEP {}------------------------------------------'.format(i))
-        # read train and validation data and train the model for n epochs
-        print 'Load train data: step {}'.format(i)
-        X_train, Y_train = cal.next_batch(BATCH_SIZE)
-
-        print 'Load val data: step {}'.format(i)
-        X_val, Y_val = cal.next_batch(BATCH_SIZE)
-
-        # TODO: Train model
-        history = model.fit(x=X_train,y=Y_train,batch_size=BATCH_SIZE,epochs=EPOCHS,validation_data=(X_val,Y_val))
-
-        train_acc = np.append(train_acc, history.history['acc'])
-        train_val_acc = np.append(train_val_acc, history.history['val_acc'])
-        train_loss = np.append(train_loss, history.history['loss'])
-        train_val_loss = np.append(train_val_loss, history.history['val_loss'])
-
-    """
     
-    
-    
-    
+    # Load the training data
     X_train, Y_train = cal.load_training()
     
+    # Load the validation data
     X_val, Y_val = cal.load_validation()
     
+    # Train model and store stats in history
     history = model.fit(x=X_train,y=Y_train,batch_size=BATCH_SIZE,
                         epochs=EPOCHS,validation_data=(X_val,Y_val))
 
+    # Append the accuracy and loss scores
     train_acc = np.append(train_acc, history.history['acc'])
     train_val_acc = np.append(train_val_acc, history.history['val_acc'])
     train_loss = np.append(train_loss, history.history['loss'])
@@ -114,10 +97,10 @@ def main():
      
     # Save model weights
     model.save('model{}.h5'.format(ID))
-    print 'model weights saved.'
+    print("model weights saved.")
 
     
-    # Create plots
+    # Plot accuracy
     plt.hold(True)
     plt.plot(train_acc)
     plt.plot(train_val_acc)
@@ -129,6 +112,7 @@ def main():
     plt.hold(False)
     plt.show()
 
+    # Plot loss
     plt.hold(True)
     plt.plot(train_loss)
     plt.plot(train_val_loss)
@@ -140,13 +124,14 @@ def main():
     plt.hold(False)
     plt.show()
     
+    # Test the model
     X_test, Y_test = cal.load_test()
     metrics = model.evaluate(x=X_test,y=Y_test, batch_size=BATCH_SIZE)
     
     print(metrics)
     print(model.metrics_names)
 
-    return
+    return 0
 
 
 if __name__ == '__main__':
