c9b590a1f3cc003ae203d500e4bdc4ac340d7cf2
Simon Fong <simonfong6@gmail.com>
2017-12-22 20:12:44 -0800

    Added saving video in native resolution, fps and flag for showing image during runtime

--- keras/live.py
+++ keras/live.py
@@ -2,6 +2,11 @@
 """
 COGS 118B Project
 Author: Simon Fong, Thinh Le, Wilson Tran
+
+Example Run Command:
+python live.py video_path model_path output_video_name flag_for_showing_image
+
+python live.py kimi.mp4 model.h5 kimi_out.mp4 False
 """
 
 from keras.models import Model, load_model
@@ -28,40 +33,55 @@ names = cal.names
 cap = None
 camera = str(sys.argv[1])
 
+# Define output video shape
+out_shape = None
+
+# Flag to show image during runtime
+SHOW_IMAGE = sys.argv[4] == 'True'
+
 if(camera == '0'):
     cap = cv2.VideoCapture(0)       # 0: Built in webcam
 elif(camera == '1'):
     cap = cv2.VideoCapture(1)       # 1: External webcam
 else:
     cap = cv2.VideoCapture(camera)  # If not webcam, then open video
-    
-# Define the codec and create VideoWriter object
+
+# Define the codec
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+# Get output video name from command line
 out_name = str(sys.argv[3])
-out = cv2.VideoWriter(out_name,fourcc, 20.0,
-                     (2*IMAGE_HEIGHT,2*IMAGE_WIDTH))
+
+# Get videocapture's shape
+out_shape = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+             int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+# Set FPS from video file
+FPS = cap.get(cv2.CAP_PROP_FPS)
+
+# Create VideoWriter object
+out = cv2.VideoWriter(out_name,fourcc,FPS,out_shape)
         
 def take_picture():
     """Takes a picture and returns it in the proper format"""
     
     # Capture frame-by-frame
-    ret, image = cap.read()
+    ret, original_image = cap.read()
     
     # Check if frame was captured
     if(ret == False):
-        return None
+        raise TypeError("Image failed to capture")
 
     # Creates an image list because the model expects 4 dimensions
     images = []
 
     # Resize image
-    image_resized = cv2.resize(image, (IMAGE_HEIGHT,IMAGE_WIDTH))
+    image_resized = cv2.resize(original_image, (IMAGE_HEIGHT,IMAGE_WIDTH))
     images.append(image_resized)
     
     # Convert list to numpy array
     images = np.array(images)
 
-    return images
+    return (images, original_image)
 
 
 def main():
@@ -70,39 +90,51 @@ def main():
     model = load_model(model_path)
     print("{} loaded".format(model_path))
     
-    # Continuously grab a frame from the camera and classify it
-    while(cap.isOpened()):
-        # Capture image from webcam
-        images = take_picture()
-        
-        # Break if image failed to capture
-        if(images is None):
-            break
-        
-        # Classify image
-        label = model.predict(images)
-        label = names[np.argmax(label[0])].split('.')[1]
-        
-        print(label)
-        
-        # Print the text on the image
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        image = cv2.resize(images[0], (2*IMAGE_HEIGHT,2*IMAGE_WIDTH))
-        cv2.putText(image,label,(50,50), font, 1,(255,0,0),2,cv2.LINE_AA)
-        
-        # Write image to video
-        out.write(image)
-        
-        
-        # Show the image
-        cv2.imshow('image',image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    try:
+        # Continuously grab a frame from the camera and classify it
+        while(cap.isOpened()):
             
-    # Release and destory everything
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+            try:
+                # Capture image from webcam
+                images, original_image = take_picture()
+            
+            except TypeError, e:
+                # Break if image failed to capture
+                print(e)
+                break
+            
+            # Classify image
+            label = model.predict(images)
+            label = names[np.argmax(label[0])].split('.')[1]
+            
+            #print(label)
+            
+            # Print the text on the image
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(original_image,label,(50,50), font, 1,(255,255,255),2,cv2.LINE_AA)
+            
+            # Write image to video
+            out.write(original_image)
+            
+            
+            # Show the image if flag is set
+            if(SHOW_IMAGE):
+                cv2.imshow('image',original_image)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                
+        # Release and destory everything
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+        print("Finished processing video, saving file to {}".format(out_name))
+    
+    except KeyboardInterrupt:
+        # Release and destory everything
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+        print("Exiting on Interrupt, saving file to {}".format(out_name))
 
 
 
