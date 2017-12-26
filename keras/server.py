7b3150bdc8cad521fc9fbc4836fa08c82f21a903
Simon Fong <simonfong6@gmail.com>
2017-12-26 14:23:41 -0800

    Working pipeline to download, classify, and display a video

--- keras/server.py
+++ keras/server.py
@@ -3,30 +3,40 @@
 Combines the youtube downloader and video classifier to download a video and 
 classify it.
 """
-from flask import Flask, request, send_file
+from flask import Flask, request, send_from_directory, render_template
 from yt_classifier import YtClassifier
+import os
 
 app = Flask(__name__)
 
+VIDEO_DIR = "videos"
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/classify', methods=['POST'])
+def classify():
     # Need dataset for label to name mapping ie. 001 --> ak47
     dataset_name = 'caltech'
     image_width,image_height = 299,299
     
     # Model to use to classify
     model_path = 'models/caltech_10_50_40_5_55.h5'
-
+    
+    url = request.form['video_url']
+    
+    
+    output_video_name = "out.mp4"
+    video_path = os.path.join(VIDEO_DIR,output_video_name)
+    
     y = YtClassifier(model_path, dataset_name, image_width, image_height)
-    yt_url = "https://www.youtube.com/watch?v=G0uGB5C56Uk"
-    output_video_name = "kimi_classified.mp4"
-    y.classify(yt_url,output_video_name)
-    return send_file(output_video_name)
+    y.classify(url,video_path)
+    return render_template('video.html', video=output_video_name)
 
-@app.route('/video/<file_name>')
-def video(file_name):
-    output_video_name = "kimi_classified.mp4"
-    return send_file(file_name)
+@app.route('/videos/<video>')
+def videos(video):
+    return send_from_directory(VIDEO_DIR,video)
 
 
 def main():
